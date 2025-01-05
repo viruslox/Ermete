dc51027499e7ecf8bf373f0e2606b132d73ee47e
viruslox <viruslox@users.noreply.github.com>
2025-01-05 13:02:38 +0100

    Create launch_ermete.sh
    A template for Ermete launch script

---
+++ launch_ermete.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#### this is just an example, depending on the use case, You might need to find Your XDG runtime folder and Xauthority
+
+## Choose the discord command prefix, copy here Your discord bot token
+export GOLIVE_BOT_PREFIX="gl."
+export GOLIVE_BOT_TOKEN="<discord token"
+
+## pulseaudio settings -> these have to match your device names
+export ERMETE_INPUT_DEVICE="Virtual.Ermete.sink.monitor"
+export ERMETE_OUTPUT_DEVICE="Virtual.Ermete.out"
+
+# Run the Go program
+echo "Starting the Go bot..."
+
+# Set here the name of Your Ermete compiled executable
+executable="Ermete"
+# Set here the path of Your Ermete compiled executable
+executable_path="/opt/Ermete/${executable}"
+
+## This prevent Ermete to be launched again if it's already running
+pids=$(pidof "$executable")
+if [[ -z "$pids" ]]; then
+	"$executable_path"
+else
+    echo "'$executable' is already running."
+fi
+
+exit 0
