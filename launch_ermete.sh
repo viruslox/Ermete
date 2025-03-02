17c5f16cef12c5e7adc8a96a284aa671cc4342e3
viruslox <viruslox@users.noreply.github.com>
2025-03-02 18:46:32 +0100

    Update launch_ermete.sh
    Including more hints into the launch script, improving logging

--- launch_ermete.sh
+++ launch_ermete.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #### this is just an example, You might add XDG runtime folder and Xauthority path
+#### I suggest call this script by crontab 
+
+LOGFILE=/opt/Ermete/rc.ermete.log
 
 ## Choose the discord command prefix, copy here Your discord bot token
 export GOLIVE_BOT_PREFIX="ermete."
@@ -7,19 +10,37 @@ export GOLIVE_BOT_TOKEN="<discord token"
 export ENABLE_AUDIO_OUTPUT="NO" # yes or NO
 
 # Run the Go program
-echo "Starting the Go bot..."
+date >> $LOGFILE
+echo "Checking the Go bot..." >> $LOGFILE
 
-# Set here the name of Your Ermete compiled executable
+# Executable file name
 executable="Ermete"
-# Set here the path of Your Ermete compiled executable
+
+# Absolute path
 executable_path="/opt/Ermete/${executable}"
 
-## This prevent Ermete to be launched again if it's already running
+# Look for the running ones
 pids=$(pidof "$executable")
-if [[ -z "$pids" ]]; then
-	"$executable_path"
-else
-    echo "'$executable' is already running."
+
+# Apparently in certain situation opus decoding goes crazy and use all available memory, swap and even tmpfs :)
+if [[ $(df -k /tmp | grep tmpfs | awk '{print $5}' | sed 's;%;;g') -gt 90 ]]; then 
+	echo "/tmp full, killing the bot" >> $LOGFILE
+	killall Ermete
+wait
+	killall ermete
+wait
 fi
+
+# If not running, finally launch the bot
+if [[ -z "$pids" ]]; then
+	date >> $LOGFILE
+	echo "Launching the bot" >> $LOGFILE
+	nohup "$executable_path" > /opt/Ermete/Ermete.log 2>&1 &
+else
+    echo "'$executable' already running." >> $LOGFILE
+fi
+
+date >> $LOGFILE
+echo "Done." >> $LOGFILE
 
 exit 0
