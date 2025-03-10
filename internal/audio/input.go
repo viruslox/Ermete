3c51b429efc1911a6753654c149009904f792bf0
viruslox <viruslox@users.noreply.github.com>
2025-03-10 22:36:11 +0100

    Update input.go
    Buffer Size Adjustment:
    We experimented with different buffer sizes (audioChan) to find a balance between real-time responsiveness and preventing audio dropouts.
    Error Handling:
    We added error checks for Opus encoding and PortAudio stream operations.
    We added timeouts to the callback function, and the opus send function.
    Audio Processing Loop:
    Added a timeout to the Opus send.
    Reused the encoded byte buffer.
    Shutdown Handling:
    Added a shutdown channel.
    Added defered log message.

--- internal/audio/input.go
+++ internal/audio/input.go
@@ -1,111 +1,126 @@
 package audio
 
 import (
-        "log"
-        "time"
+	"log"
+	"time"
 
-        "github.com/bwmarrin/discordgo"
-        "github.com/gordonklaus/portaudio"
-        "github.com/hraban/opus"
+	"github.com/bwmarrin/discordgo"
+	"github.com/gordonklaus/portaudio"
+	"github.com/hraban/opus"
 )
 
 type PortAudioInput struct {
-        stream      *portaudio.Stream
-        vc          *discordgo.VoiceConnection
-        opusEncoder *opus.Encoder
-        audioChan   chan []float32
+	stream      *portaudio.Stream
+	vc          *discordgo.VoiceConnection
+	opusEncoder *opus.Encoder
+	audioChan   chan []float32
 }
 
 var inputDevice *PortAudioInput
 
 func StartInput(vc *discordgo.VoiceConnection, shutdownChan <-chan struct{}) error {
-        inputDevice = &PortAudioInput{} // Initialize here
-        return inputDevice.startProcessing(vc, shutdownChan)
+	inputDevice = &PortAudioInput{}
+	return inputDevice.startProcessing(vc, shutdownChan)
 }
 
 func StopInput() error {
-        if inputDevice != nil {
-                return inputDevice.Stop()
-        }
-        return nil
+	if inputDevice != nil {
+		return inputDevice.Stop()
+	}
+	return nil
 }
 
 func (pi *PortAudioInput) startProcessing(vc *discordgo.VoiceConnection, shutdownChan <-chan struct{}) error {
-    pi.vc = vc
-    pi.audioChan = make(chan []float32, 100)
-    var err error
-    pi.opusEncoder, err = opus.NewEncoder(48000, 2, opus.AppAudio)
-    if err != nil {
-        return err
-    }
-    pi.opusEncoder.SetBitrate(96000)
-    device, err := portaudio.DefaultInputDevice()
-    if err != nil {
-        return err
-    }
+	pi.vc = vc
+	pi.audioChan = make(chan []float32, 80) // buffer size, till 500 works good
 
-    inputParams := portaudio.StreamParameters{
-        Input:         portaudio.StreamDeviceParameters{Device: device, Channels: 2},
-        SampleRate:    48000,
-        FramesPerBuffer: 960,
-    }
+	var err error
+	pi.opusEncoder, err = opus.NewEncoder(48000, 2, opus.AppAudio)
+	if err != nil {
+		return err
+	}
+	pi.opusEncoder.SetBitrate(96000)
 
-    pi.stream, err = portaudio.OpenStream(inputParams, pi.callback)
-    if err != nil {
-        return err
-    }
+	device, err := portaudio.DefaultInputDevice()
+	if err != nil {
+		return err
+	}
 
-    err = pi.stream.Start()
-    if err != nil {
-        return err
-    }
+	inputParams := portaudio.StreamParameters{
+		Input: portaudio.StreamDeviceParameters{
+			Device:   device,
+			Channels: 2, // Stereo input
+		},
+		SampleRate:    48000,
+		FramesPerBuffer: 960,
+	}
 
-    go pi.processAudio(shutdownChan)
+	pi.stream, err = portaudio.OpenStream(inputParams, pi.callback)
+	if err != nil {
+		return err
+	}
 
-    return nil
+	if err := pi.stream.Start(); err != nil {
+		return err
+	}
+
+	go pi.processAudio(shutdownChan)
+
+	return nil
 }
 
 func (pi *PortAudioInput) callback(in []float32, out []float32) {
-        buffer := make([]float32, len(in))
-        copy(buffer, in)
+	buffer := make([]float32, len(in))
+	copy(buffer, in)
 
-        select {
-        case pi.audioChan <- buffer:
-                // Successfully added to buffer
-        default:
-                log.Println("Audio buffer is full, dropping frame")
-        }
+	select {
+	case pi.audioChan <- buffer:
+		// Successfully added to buffer
+	case <-time.After(50 * time.Millisecond):
+		log.Println("AudioIn Timeout while adding to audio buffer, dropping frame")
+	}
 }
 
 func (pi *PortAudioInput) processAudio(shutdownChan <-chan struct{}) {
-        for {
-                select {
-                case frame := <-pi.audioChan:
-                        encoded := make([]byte, 2048)
+	defer log.Println("AudioIn Audio processing goroutine exited")
+	encoded := make([]byte, 2048) // Reuse buffer
 
-                        n, err := pi.opusEncoder.EncodeFloat32(frame, encoded)
-                        if err != nil {
-                                log.Println("Error encoding audio:", err)
-                                continue
-                        }
+	for {
+		select {
+		case frame, ok := <-pi.audioChan:
+			if !ok {
+				log.Println("AudioIn Audio channel closed, stopping processing")
+				return
+			}
 
-                        select {
-                        case pi.vc.OpusSend <- encoded[:n]: // Send encoded data
-                                time.Sleep(5 * time.Millisecond)
-                        default:
-                                log.Println("OpusSend channel is full, dropping frame")
-                        }
-                case <-shutdownChan:
-                        log.Println("Shutting down audio input processing")
-                        return
-                }
-        }
+			n, err := pi.opusEncoder.EncodeFloat32(frame, encoded)
+			if err != nil {
+				log.Println("AudioIn Error encoding audio:", err)
+				continue
+			}
+
+			select {
+			case pi.vc.OpusSend <- encoded[:n]: // Send encoded data
+				time.Sleep(5 * time.Millisecond) // Throttle sending
+			case <-time.After(50 * time.Millisecond):
+				log.Println("AudioIn Timeout while sending Opus data, dropping frame")
+			}
+
+		case <-shutdownChan:
+			log.Println("AudioIn Shutting down audio input processing")
+			return
+		}
+	}
 }
 
 func (pi *PortAudioInput) Stop() error {
-        if pi.stream != nil {
-                close(pi.audioChan) // Close the audio channel
-                return pi.stream.Close()
-        }
-        return nil
+	if pi.stream != nil {
+		close(pi.audioChan) // Close the audio channel
+		err := pi.stream.Close()
+		if err != nil {
+			return err
+		}
+		pi.stream = nil
+	}
+	return nil
 }
